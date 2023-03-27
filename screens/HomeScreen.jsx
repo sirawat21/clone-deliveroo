176e76645a28f8ab69f9038788b25b071d8d019b
Brett Sira <sirawat21@gmail.com>
2023-03-28 10:41:27 +1100

    Implemented Hero icon into Home screen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView, View, Text, Image } from "react-native";
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,20 +14,27 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text className="text-red-500"></Text>
-      {/* Header */}
-      <View>
-        <Image
-          source={{
-            url: "https://links.papareact.com/wru",
-          }}
-          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-        />
-        <View>
-          <Text className="font-bold text-xs text-gray-400">Deliver Now!</Text>
-          <Text className="font-bold text-xl">Current Location</Text>
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              url: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View>
+            <Text className="font-bold text-xs text-gray-400">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+          <UserIcon size={35} color="#00CCBB" />
         </View>
-      </View>
+      </Text>
     </SafeAreaView>
   );
 };
