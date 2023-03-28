6ddac2b8cb781e64e289610cc9672a1c8ec65824
Brett Sira <sirawat21@gmail.com>
2023-03-28 16:24:04 +1100

    Restructure files

---
+++ components/categories/categories.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-natives";
+
+const Categroies = () => {
+    return(
+        <View>
+            <Text>Categories component</Text>
+        </View>
+    );
+};
+
+export default Categroies;
