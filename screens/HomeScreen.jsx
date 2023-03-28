f1bd7468a011f167f8af6dacda63328e32865464
Brett Sira <sirawat21@gmail.com>
2023-03-28 16:16:22 +1100

    Added search box

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,7 +1,12 @@
 import { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaView, View, Text, Image } from "react-native";
-import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
+import { SafeAreaView, View, Text, Image, TextInput } from "react-native";
+import {
+  AdjustmentsVerticalIcon,
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,15 +18,15 @@ const HomeScreen = () => {
   });
 
   return (
-    <SafeAreaView className="bg-white pt-5">
+    <SafeAreaView className="bg-white pt-5 h-auto">
       <Text className="text-red-500">
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
           <Image
             source={{
               url: "https://links.papareact.com/wru",
             }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            className="h-4 w-7 bg-gray-300 p-4 rounded-full"
           />
           <View className="flex-1">
             <Text className="font-bold text-xs text-gray-400">
@@ -35,6 +40,17 @@ const HomeScreen = () => {
           <UserIcon size={35} color="#00CCBB" />
         </View>
         {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row space-x-2 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color="gray" size={20} />
+            <TextInput
+              className="w-[85%]"
+              placeholder="Restaurants and cuisines"
+              keyboardType="default"
+            />
+          </View>
+          <AdjustmentsVerticalIcon color="#00CCBB" />
+        </View>
       </Text>
     </SafeAreaView>
   );
