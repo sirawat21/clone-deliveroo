680674bb15634710afd51dd4ef7f467aad7834fd
Brett Sira <sirawat21@gmail.com>
2023-04-02 15:39:48 +1000

    Added detail and styling RestaurantCard component

--- components/restaurant-card/restaurant-card.component.jsx
+++ components/restaurant-card/restaurant-card.component.jsx
@@ -1,5 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import { StarIcon, LocationMarkerIcon } from "react-native-heroicons/outline";
 
 const RestaurantCard = ({
   id,
@@ -14,19 +15,23 @@ const RestaurantCard = ({
   lat,
 }) => {
   return (
-    <View>
-      <Text>restaurant-card.component</Text>
-      <Text>{id}</Text>
-      <Text>{imgUrl}</Text>
-      <Text>{title}</Text>
-      <Text>{rating}</Text>
-      <Text>{genre}</Text>
-      <Text>{address}</Text>
-      <Text>{short_description}</Text>
-      <Text>{dishes}</Text>
-      <Text>{long}</Text>
-      <Text>{lat}</Text>
-    </View>
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image source={{ uri: imgUrl }} className="h-36 w-64 rounded-sm" />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text> · {genre}
+          </Text>
+        </View>
+        
+        <View>
+          <LocationMarkerIcon color="gray" opacity={0.4} size={22}/>
+          <Text className="text-xs text-gray-500">Near by · {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
