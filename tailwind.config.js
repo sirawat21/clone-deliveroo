a9348cba617c790f77555450865d1e7229208ba6
Brett Sira <sirawat21@gmail.com>
2023-04-07 22:01:24 +1000

    Installed Eslint Airbnb config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './App.{js,jsx,ts,tsx}',
+    './screens/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: ["nativewind/babel"],
+  plugins: ['nativewind/babel'],
 };
